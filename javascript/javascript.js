4995eca3336e4b9249763ba6e58851e242043d88
alnk-ops <alnk05711@gmail.com>
2025-12-29 15:44:48 +0530

    today last commit

---
+++ javascript/javascript.js
@@ -0,0 +1,35 @@
+// document.write("This is an external JavaScript file!");
+// var a=10;
+// var b="abhijith";
+// var c=20;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(a+c);
+// console.log(a-c);
+// console.log(a*c);
+// console.log(a/c);
+
+// // comparison operators
+// console.log(a==c);
+// console.log(a!=c);
+// console.log(a===c);
+// console.log(a!==c);
+// console.log(a>c);
+// console.log(a<c);
+// console.log(a>=c);
+// console.log(a<=c);
+
+// // logical operators
+
+// console.log((a<c) && (a==c));
+// console.log((a<c) || (a==c));
+// console.log(!(a<c));
+
+let score = 85;
+
+if (score >= 90) {
+    console.log("Grade: A");
+} else{
+    console.log("Grade: B");
+}
+console.log(`Your score is : ${score}`);
