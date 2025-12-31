3593ac43d70454a96c1754835c132fab3a9020d8
alnk-ops <alnk05711@gmail.com>
2025-12-31 12:44:08 +0530

    morning commit

--- javascript1/javascript2.js
+++ javascript1/javascript2.js
@@ -33,3 +33,32 @@ console.log("subtraction result is:",subtract(10,4));
 console.log("The value of d is:",d);
 var d=50;
 console.log("The value of d after initialization is:",d);
+
+if(true){
+    var x=10;
+}
+console.log(x);
+// block scope example
+if(true){
+    let y=20;
+    console.log("The value of y inside block is:",y);
+}
+
+function greet(name="Guest"){
+    console.log(name)
+}
+greet();
+
+// // array of functions
+// let operations=[
+//     function(x,y){return x+y;}, 
+//     function(x,y){return x-y;},
+//     function(x,y){return x*y;},
+//     function(x,y){return x/y;}
+// ];  
+// console.log("Addition:",operations[0](10,5));
+// spread operator
+function sumAll(...numbers){
+   let total=0;
+   for const num of numbers
+}
---
+++ script/script.js
@@ -0,0 +1,58 @@
+// import {add ,PI } from "./script2.js";
+
+// console.log(add(2,3));
+// console.log(PI);
+
+class animal{
+    constructor(name,sound){
+        this.name=name;
+        this.sound=sound;
+    }
+
+}
+class dog extends animal{
+    constructor(name,sound,breed){
+        super(name,sound);
+        this.breed=breed;
+    }       
+
+}
+let dog1=new dog("tommy","bark","bulldog");
+console.log(dog1);
+
+// function showmessage(){
+//     console.log("hello world");
+// }
+// setTimeout(showmessage,2000);
+// setTimeout(function(){
+//     console.log("hello world");
+// },3000);
+
+// printing numbers from 1 to 5 after every 1 second
+// for(let i=1;i<=5;i++){
+//     setTimeout(function(){
+//         console.log(i);
+//     },i*1000);      
+// }
+
+// setInterval
+// let count=1;
+// let intervalId=setInterval(function(){
+//     console.log("count: "+count);
+//     count++;
+// },1000);
+
+// to stop the interval after 5 counts
+// setTimeout(function(){
+//     clearInterval(intervalId);
+//     console.log("Interval cleared");
+// },16000);
+
+const output=document.getElementById("Heading").innerHTML = "Hello World!";
+console.log(output);
+
+
+
+
+
+
